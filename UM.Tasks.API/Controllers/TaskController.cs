5902c1f73c12e28bc2b1c9585f901fdaef7e1f90
golybock <teplovartem3094@gmail.com>
2024-02-15 23:59:24 +0300

    finally 2 week api

--- UM.Tasks.API/Controllers/TaskController.cs
+++ UM.Tasks.API/Controllers/TaskController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UM.Models.Tasks.Blank;
 using UM.Tasks.Services.Services.Task;
 
 namespace UM.Tasks.API.Controllers;
@@ -7,12 +8,63 @@ namespace UM.Tasks.API.Controllers;
 [Route("api/[controller]")]
 public class TaskController : ControllerBase
 {
-	private ITaskService _taskService;
+	private readonly ITaskService _taskService;
 
 	public TaskController(ITaskService taskService)
 	{
 		_taskService = taskService;
 	}
 
+	[HttpGet("[action]")]
+	public async Task<IActionResult> GetTasksAsync()
+	{
+		var tasks = await _taskService.GetTasksAsync();
 
+		return Ok(tasks);
+	}
+
+	[HttpGet("[action]")]
+	public async Task<IActionResult> GetEmptyTasksAsync()
+	{
+		var tasks = await _taskService.GetEmptyTasksAsync();
+
+		return Ok(tasks);
+	}
+
+	[HttpGet("[action]")]
+	public async Task<IActionResult> GetTaskAsync(Guid id)
+	{
+		var task = await _taskService.GetTaskAsync(id);
+
+		if(task == null)
+		{
+			return NotFound();
+		}
+
+		return Ok(task);
+	}
+
+	[HttpPost("[action]")]
+	public async Task<IActionResult> CreateTaskAsync(TaskBlank task)
+	{
+		var result = await _taskService.CreateTaskAsync(task);
+
+		return result ? Ok() : BadRequest();
+	}
+
+	[HttpPut("[action]")]
+	public async Task<IActionResult> UpdateTaskAsync(Guid id, TaskBlank task)
+	{
+		var result = await _taskService.UpdateTaskAsync(id, task);
+
+		return result ? Ok() : BadRequest();
+	}
+
+	[HttpDelete("[action]")]
+	public async Task<IActionResult> DeleteTaskAsync(Guid id)
+	{
+		var result = await _taskService.DeleteTaskAsync(id);
+
+		return result ? Ok() : BadRequest();
+	}
 }
---
+++ UM.Tasks.API/Controllers/WorkerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UM.Models.Tasks.Blank;
+using UM.Tasks.Services.Services.Worker;
+
+namespace UM.Tasks.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WorkerController : ControllerBase
+{
+	private readonly IWorkerService _workerService;
+
+	public WorkerController(IWorkerService workerService)
+	{
+		_workerService = workerService;
+	}
+
+	public async Task<IActionResult> GetWorkersAsync()
+	{
+		var workers = await _workerService.GetWorkersAsync();
+		return Ok(workers);
+	}
+
+	public async Task<IActionResult> GetWorkerAsync(string login, string password)
+	{
+		var worker = await _workerService.GetWorkerAsync(login, password);
+
+		if (worker == null)
+		{
+			return NotFound();
+		}
+
+		return Ok(worker);
+	}
+
+	public async Task<IActionResult> CreateWorkerAsync(WorkerBlank workerBlank)
+	{
+		var result = await _workerService.CreateWorkerAsync(workerBlank);
+
+		return result ? Ok() : BadRequest();
+	}
+}
