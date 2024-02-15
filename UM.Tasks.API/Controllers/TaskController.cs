f6c042bf71de9bd0d82d5840b499717e8fed8a51
golybock <teplovartem3094@gmail.com>
2024-02-15 18:23:44 +0300

    created controller

---
+++ UM.Tasks.API/Controllers/TaskController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UM.Tasks.Services.Services.Task;
+
+namespace UM.Tasks.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TaskController : ControllerBase
+{
+	private ITaskService _taskService;
+
+	public TaskController(ITaskService taskService)
+	{
+		_taskService = taskService;
+	}
+
+
+}
