1c752edf9ef8bc2ea7ca5ec23d0922a065fc350b
golybock <teplovartem3094@gmail.com>
2024-02-20 00:42:12 +0300

    aaaaaaaaaaaaaaa

---
+++ TaskWorkerApp/Data/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskWorkerApp.Data;
+
+public partial class Task
+{
+    public Guid Id { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public Guid? WorkerId { get; set; }
+
+    public int? Priority { get; set; }
+
+    public int? Status { get; set; }
+
+    public DateTime CreatedTime { get; set; }
+
+    public DateTime? StartTime { get; set; }
+
+    public DateTime? EndTime { get; set; }
+
+    public string? ClientEmail { get; set; }
+
+    public string? Solution { get; set; }
+
+    public string? Version { get; set; }
+
+    public virtual Worker? Worker { get; set; }
+}
---
+++ TaskWorkerApp/Data/VersionDependency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskWorkerApp.Data;
+
+public partial class VersionDependency
+{
+    public int Id { get; set; }
+
+    public Guid VersionId { get; set; }
+
+    public Guid DependencyId { get; set; }
+
+    public virtual Dependency Dependency { get; set; } = null!;
+
+    public virtual Version Version { get; set; } = null!;
+}
