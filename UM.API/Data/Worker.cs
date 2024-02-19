c9b263d821cf54aa3fd68d275bad235921348dc8
golybock <teplovartem3094@gmail.com>
2024-02-19 21:22:37 +0300

    ABOBA

---
+++ UM.API/Data/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UM.API.Data;
+
+public partial class Worker
+{
+    public Guid Id { get; set; }
+
+    public int RoleId { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+
+    public byte[] Password { get; set; } = null!;
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+}
